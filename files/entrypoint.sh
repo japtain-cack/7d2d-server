7a3e72d3f990d6adab925aad7fef40f96b739a59
Nathan Snow <nathan.snow@mimir-tech.org>
2021-12-31 21:09:35 +0000

    updating steamcli script

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -11,8 +11,8 @@ sudo chown -R sevend2d:sevend2d ${SEVEND2D_HOME}
 cat <<EOF> ${SEVEND2D_HOME}/sevend2d.conf
 @ShutdownOnFailedCommand 1 //set to 0 if updating multiple servers at once
 @NoPromptForPassword 1
-login anonymous
 force_install_dir ${SEVEND2D_HOME}/server/
+login anonymous
 app_update 294420
 quit
 EOF
